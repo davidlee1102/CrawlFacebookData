82eeb8477667d2836efef50060e55c8b2b1d91e9
davidlee1102 <lenhathungdev112@gmail.com>
2022-12-16 19:58:55 +0700

    Update requirements

--- EVNCrawlFacebook/urls.py
+++ EVNCrawlFacebook/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
 from .views import health_check, facebook_data_crawl_request, file_request
-from django.conf.urls import (
-    handler400, handler403, handler404, handler500
-)
 
 urlpatterns = [
     path('comment_check/', facebook_data_crawl_request.get_comment),
     path('general_info_check/', facebook_data_crawl_request.get_general_information),
     path('', health_check.health_check),
-    # path('comment_check/download_file/', file_request.download_file),
+    path('download_file/', file_request.download_file),
 ]
