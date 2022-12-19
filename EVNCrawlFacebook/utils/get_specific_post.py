60e9e66a947caf811e54a4ec360e53a431197052
davidlee1102 <lenhathungdev112@gmail.com>
2022-12-19 18:29:35 +0700

    Update get specific post

--- EVNCrawlFacebook/utils/get_specific_post.py
+++ EVNCrawlFacebook/utils/get_specific_post.py
@@ -1,13 +1,15 @@
+import pprint
+
 from selenium.webdriver.common.by import By
 
 from EVNCrawlFacebook.selenium.utils import facebook_login
 from EVNCrawlFacebook.utils import pre_process_selenium
 
 import time
+import json
 
 
 def crawler_specific_info(post_link: str, post_id: str):
-    general_info = {}
     driver = facebook_login.login()
     post_link = post_link.replace("mbasic", "m")
     driver.get(post_link)
@@ -16,32 +18,58 @@ def crawler_specific_info(post_link: str, post_id: str):
     interaction_link = driver.find_element(by=By.XPATH, value=xpath_address).get_attribute('href')
     driver.get(interaction_link)
 
-    try:
-        xpath_address = f'//*[@id="reaction_profile_pager"]/a/div/div/div/strong'
-        check_still = driver.find_element(by=By.XPATH, value=xpath_address)
-        while "xem" in check_still.text.lower():
-            check_still.click()
-            time.sleep(2)
-            check_still = driver.find_element(by=By.XPATH, value=xpath_address)
-    except:
-        pass
+    specific_info_list = []
 
-    xpath_address = f'//*[@id="reaction_profile_browser"]/div'
-    reaction_info_list = driver.find_elements(by=By.XPATH, value=xpath_address)
-    print(len(reaction_info_list))
+    xpath_address = f'/html/body/div[1]/div/div[4]/div/div[1]/div/div/div[1]/div/div/div/span'
+    reaction_type_list = driver.find_elements(by=By.XPATH, value=xpath_address)
+    for reaction_type in reaction_type_list:
+        reaction_type.click()
+        time.sleep(2)
+        try:
+            # need to fix error reaction type
+            # try:
+            #     reaction_type_text = \
+            #     reaction_type.find_element(by=By.TAG_NAME, value='span').get_attribute('area-label').split(" ")[-1]
+            #     print(reaction_type_text)
+            # except:
+            #     print('error here')
+            reaction_type = json.loads(reaction_type.get_attribute('data-store'))
+            if reaction_type.get("reactionID", "") is 'all':
+                continue
 
-    #
-    # for reaction_info in reaction_info_list:
-    #     try:
-    #         if "Xem" in reaction_info.find_element(by=By.TAG_NAME, value='span').text:
-    #             print(reaction_info.find_element(by=By.TAG_NAME, value='a').get_attribute('href'))
-    #         else:
-    #             continue
-    #     except:
-    #
-    #         continue
-    time.sleep(5000)
-    return None
+            else:
+
+                reaction_id = reaction_type.get("reactionID", "")
+                try:
+                    xpath_address = f'//*[@id="reaction_profile_pager{reaction_id}"]/a/div/div/div/strong'
+                    check_still = driver.find_element(by=By.XPATH, value=xpath_address)
+                    while "xem" in check_still.text.lower():
+                        check_still.click()
+                        time.sleep(2)
+                        check_still = driver.find_element(by=By.XPATH, value=xpath_address)
+                except:
+                    pass
+
+                xpath_address = f'//*[@id="reaction_profile_browser{reaction_id}"]/div'
+                reaction_info_list = driver.find_elements(by=By.XPATH, value=xpath_address)
+                for reaction_info in reaction_info_list:
+                    specific_info_json = {}
+
+                    specific_info_json['reaction_type'] = reaction_id
+                    try:
+                        specific_info_json['user_info_name'] = reaction_info.find_element(by=By.TAG_NAME,
+                                                                                          value='strong').text.strip()
+                        specific_info_json['user_info_link'] = \
+                            reaction_info.find_element(by=By.TAG_NAME,
+                                                       value='a').get_attribute('href').replace("m.", "").split(
+                                'groupid=')[
+                                0].strip()[:-1]
+                    except:
+                        continue
+                    specific_info_list.append(specific_info_json)
+        except:
+            continue
+    return specific_info_list
 
 
 def process_specific_info(post_link: str):
